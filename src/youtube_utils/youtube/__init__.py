322802dee66d29443651e4ccc925054bc37d2591
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-04-09 12:47:06 +0545

    feat: use yt-dlp

--- src/youtube_utils/youtube/__init__.py
+++ src/youtube_utils/youtube/__init__.py
@@ -1,6 +1,14 @@
+from yt_dlp import YoutubeDL
+from pathlib import Path
+
 class Youtube:
+    path = Path(__file__).parent
     def __init__(self, browser: str):
         self.browser = browser
+        self.cookies_from_browser = (browser, )
     
     def __repr__(self):
         return self.browser
+    
+    def yt_dlp(self):
+        return YoutubeDL({ 'cookiesfrombrowser': self.cookies_from_browser,})
