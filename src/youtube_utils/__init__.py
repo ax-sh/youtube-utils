dfd397c7ad9a8277b2d8bc900c51885925c307e9
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-04-09 16:18:03 +0545

    refactor: maintenance

--- src/youtube_utils/__init__.py
+++ src/youtube_utils/__init__.py
@@ -2,6 +2,48 @@ from yt_dlp import YoutubeDL
 from .utils import Path
 
 
+class WatchLater:
+    def __init__(self, data):
+        self.data = data
+
+    def raw_data(self):
+        return self.data
+
+    @staticmethod
+    def process_entry(entry):
+        # {
+        #     "description": null,
+
+        #     "channel_id": "UCNTVzV1InxHV-YR0fSajqPQ",
+
+        #     "uploader": "Supabase",
+        #     "uploader_id": "@Supabase",
+        #     "uploader_url": "https://www.youtube.com/@Supabase",
+
+        #     "release_timestamp": null,
+        #     "availability": null,
+
+        #     "live_status": null,
+        #     "channel_is_verified": null,
+        #     "__x_forwarded_for_ip": null
+        # },
+        thumb = max(entry["thumbnails"], key=lambda x: x["height"] * x["width"])
+        return {
+            "id": entry["id"],
+            "url": entry["url"],
+            "upload_date": entry["timestamp"],
+            "channel": entry["channel"],
+            "channel_url": entry["channel_url"],
+            "title": entry["title"],
+            "duration": entry["duration"],
+            "view_count": entry["view_count"],
+            "thumbnail": thumb["url"],
+        }
+
+    def items(self):
+        return list(map(self.process_entry, self.data["entries"]))
+
+
 class Youtube:
     WATCH_LATER_URL = "https://www.youtube.com/playlist?list=WL"
     path = Path(__file__).parent
@@ -24,16 +66,16 @@ class Youtube:
     # @see https://github.com/yt-dlp/yt-dlp/blob/master/yt_dlp/YoutubeDL.py
     def watch_later(self):
         with self.yt_dlp(
-            {
-                "extract_flat": "in_playlist",
-                "dump_single_json": True,
-                "allow_unplayable_formats": True,
-                "ignoreerrors": True,
-                "no_warnings": True,
-                "clean_infojson": True,
-                #  E.g. {'youtube': {'skip': ['dash', 'hls']}}
-                "extractor_args": {"youtubetab": {"approximate_date": [""]}},
-            }
+                {
+                    "extract_flat": "in_playlist",
+                    "dump_single_json": True,
+                    "allow_unplayable_formats": True,
+                    "ignoreerrors": True,
+                    "no_warnings": True,
+                    "clean_infojson": True,
+                    #  E.g. {'youtube': {'skip': ['dash', 'hls']}}
+                    "extractor_args": {"youtubetab": {"approximate_date": [""]}},
+                }
         ) as yt:
             info = yt.extract_info(self.WATCH_LATER_URL, download=False)
-            return info
+            return WatchLater(info)
--- src/youtube_utils/run.py
+++ src/youtube_utils/run.py
@@ -3,45 +3,14 @@ import sqlite3
 import pandas as pd
 
 
-def process_entries(entry):
-    # {
-    #     "description": null,
-
-    #     "channel_id": "UCNTVzV1InxHV-YR0fSajqPQ",
-
-    #     "uploader": "Supabase",
-    #     "uploader_id": "@Supabase",
-    #     "uploader_url": "https://www.youtube.com/@Supabase",
-
-    #     "release_timestamp": null,
-    #     "availability": null,
-
-    #     "live_status": null,
-    #     "channel_is_verified": null,
-    #     "__x_forwarded_for_ip": null
-    # },
-    thumb = max(entry["thumbnails"], key=lambda x: x["height"] * x["width"])
-    return {
-        "id": entry["id"],
-        "url": entry["url"],
-        "upload_date": entry["timestamp"],
-        "channel": entry["channel"],
-        "channel_url": entry["channel_url"],
-        "title": entry["title"],
-        "duration": entry["duration"],
-        "view_count": entry["view_count"],
-        "thumbnail": thumb["url"],
-    }
-
-
 def main():
     yt = Youtube("vivaldi")
     folder = Path(".cache")
 
     watch_later = yt.watch_later()
-    entries = list(map(process_entries, watch_later["entries"]))
+    entries = watch_later.items()
     raw = folder / "watch-later.entries.json"
-    raw.write_json(watch_later)
+    raw.write_json(watch_later.raw_data())
     df = pd.DataFrame(entries)
     # df.to_csv('data.csv', index=False)
     conn = sqlite3.connect(folder / "watch_later.db")
