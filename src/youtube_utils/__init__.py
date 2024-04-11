2dc6636959338e0f237e1a11055cae26c52a461c
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-04-11 10:54:18 +0545

    feat: made faster with hook support

--- src/youtube_utils/__init__.py
+++ src/youtube_utils/__init__.py
@@ -4,13 +4,6 @@ from .logger import YoutubeUtilsLogger
 from .utils import Path
 
 from .watchlater import WatchLater
-from pprint import pprint
-# ℹ️ See help(yt_dlp.postprocessor.PostProcessor)
-class YoutubeUtilPP(postprocessor.PostProcessor):
-    def run(self, info):
-        # self.to_screen('Doing stuff')
-        pprint(info)
-        return [], info
 
 class Youtube:
     WATCH_LATER_URL = "https://www.youtube.com/playlist?list=WL"
@@ -23,7 +16,9 @@ class Youtube:
     def __repr__(self):
         return self.browser
 
-    def yt_dlp(self, updated_options={}):
+    def yt_dlp(self, updated_options=None):
+        if updated_options is None:
+            updated_options = {}
         options = {
             "cookiesfrombrowser": self.cookies_from_browser
         }
@@ -33,12 +28,18 @@ class Youtube:
     def make_api_with_session(self):
         cookies = self.yt_dlp().cookiejar
         api = ''
+
+        # api = self.make_api_with_session()
+        # video_id = 'QQPz3eXXgQI'
+        # api.login()
+        # api.remove_video_id_from_playlist('WL', video_id)
+        # exit()
         # api = youtube_unofficial.YouTube(cookiejar_cls=cookies, logged_in=True)
        
         return api
 
     # @see https://github.com/yt-dlp/yt-dlp/blob/master/yt_dlp/YoutubeDL.py
-    def watch_later(self):
+    def watch_later(self, postprocessor=None):
         with self.yt_dlp(
                 {
                     "extract_flat": "in_playlist",
@@ -47,19 +48,13 @@ class Youtube:
                     "ignoreerrors": False,
                     "no_warnings": True,
                     "clean_infojson": True,
-                    "lazy_playlist":True,
-
+                    "lazy_playlist": True,
                     'logger': YoutubeUtilsLogger(),
                     #  E.g. {'youtube': {'skip': ['dash', 'hls']}}
                     "extractor_args": {"youtubetab": {"approximate_date": [""]}},
                 }
         ) as yt:
-            
-            # api = self.make_api_with_session()
-            # video_id = 'QQPz3eXXgQI'
-            # api.login()
-            # api.remove_video_id_from_playlist('WL', video_id)
-            # exit()
-            # yt.add_post_processor(YoutubeUtilPP(), when='pre_process')
+            if postprocessor is not None:
+                yt.add_post_processor(postprocessor, when='pre_process')
             info = yt.extract_info(self.WATCH_LATER_URL, download=False)
             return WatchLater(info)
